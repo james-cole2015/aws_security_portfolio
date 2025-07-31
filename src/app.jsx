4b1e70d3de473e5ae841f6925a7d6516bdee1388
brooks <brooks.houck@gmail.com>
2025-07-30 21:11:02 -0400

    adding src files

---
+++ src/app.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Resume from './pages/Resume'
+import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main className="main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
